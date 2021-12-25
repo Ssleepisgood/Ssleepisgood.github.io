3de8c41e57100db159ea0cc501a463f5fe35b537
Ssleepisgood <mattcjanke@gmail.com>
2021-12-25 22:03:39 +0000

    update

--- script.js
+++ script.js
@@ -1,157 +1,34 @@
-// Option 1: Import the entire three.js core library.
-import * as THREE from 'three';
-
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer({ antialias: true});
 
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-scene.add(ambientLight);
+var geometry = new THREE.BoxGeometry( 1, 1, 1);
+var material = new THREE.MeshStandardMaterial( { color: 0xff0051 });
+var cube = new THREE.Mesh ( geometry, material );
+scene.add( cube );
+renderer.render( scene, camera );
+camera.position.z = 5;
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-directionalLight.position.set(200, 500, 300);
-scene.add(directionalLight); 
+function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.04;
+    cube.rotation.y += 0.04;
+    renderer.render( scene, camera );
+   }
+animate();
 
+var ambientLight = new THREE.AmbientLight ( 0xffffff, 0.5);
+scene.add( ambientLight );
 
-// Setting up camera
-const aspectRatio = window.innerWidth / window.innerHeight;
-const cameraWidth = 150;
-const cameraHeight = cameraWidth / aspectRatio;
+var pointLight = new THREE.PointLight( 0xffffff, 1 );
+pointLight.position.set( 25, 50, 25 );
+scene.add( pointLight );
 
-const camera = new THREE.OrthographicCamera(
-  cameraWidth / -2, // left
-  cameraWidth / 2, // right
-  cameraHeight / 2, // top
-  cameraHeight / -2, // bottom
-  0, // near plane
-  1000 // far plane
-);
-camera.position.set(200, 200, 200);
-camera.lookAt(0, 10, 0);
+var geometry = new THREE.BoxGeometry( 3, 3, 3);
+var material = new THREE.MeshBasicMaterial( {color: "#dadada", wireframe: true, transparent: true});
+var wireframeCube = new THREE.Mesh ( geometry, material );
+scene.add( wireframeCube );
 
-function createWheels() {
-    const geometry = new THREE.BoxBufferGeometry(12, 12, 33);
-    const material = new THREE.MeshLambertMaterial({ color: 0x333333 });
-    const wheel = new THREE.Mesh(geometry, material);
-    return wheel;
-  }
-
-  function createCar() {
-    const car = new THREE.Group();
-    
-    const backWheel = createWheels();
-    backWheel.position.y = 6;
-    backWheel.position.x = -18;
-    car.add(backWheel);
-    
-    const frontWheel = createWheels();
-    frontWheel.position.y = 6;  
-    frontWheel.position.x = 18;
-    car.add(frontWheel);
-  
-    const main = new THREE.Mesh(
-      new THREE.BoxBufferGeometry(60, 15, 30),
-      new THREE.MeshLambertMaterial({ color: 0x78b14b })
-    );
-    main.position.y = 12;
-    car.add(main);
-  
-    const cabin = new THREE.Mesh(
-      new THREE.BoxBufferGeometry(33, 12, 24),
-      new THREE.MeshLambertMaterial({ color: 0xffffff })
-    );
-    cabin.position.x = -6;
-    cabin.position.y = 25.5;
-    car.add(cabin);
-  
-    return car;
-  }
-  
-  const car = createCar();
-  scene.add(car);
-
-  function getCarFrontTexture() {
-    const canvas = document.createElement("canvas");
-    canvas.width = 64;
-    canvas.height = 32;
-    const context = canvas.getContext("2d");
-  
-    context.fillStyle = "#ffffff";
-    context.fillRect(0, 0, 64, 32);
-  
-    context.fillStyle = "#666666";
-    context.fillRect(8, 8, 48, 24);
-  
-    return new THREE.CanvasTexture(canvas);
-  }
-
-// Set up renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
-
-document.body.appendChild(renderer.domElement);
-
-
-function getCarSideTexture() {
-    const canvas = document.createElement("canvas");
-    canvas.width = 128;
-    canvas.height = 32;
-    const context = canvas.getContext("2d");
-  
-    context.fillStyle = "#ffffff";
-    context.fillRect(0, 0, 128, 32);
-  
-    context.fillStyle = "#666666";
-    context.fillRect(10, 8, 38, 24);
-    context.fillRect(58, 8, 60, 24);
-  
-    return new THREE.CanvasTexture(canvas);
-  }
-  
-  renderer.render(scene, camera);
-
-  function createCar() {
-    const car = new THREE.Group();
-  
-    const backWheel = createWheels();
-    backWheel.position.y = 6;
-    backWheel.position.x = -18;
-    car.add(backWheel);
-  
-    const frontWheel = createWheels();
-    frontWheel.position.y = 6;
-    frontWheel.position.x = 18;
-    car.add(frontWheel);
-  
-    const main = new THREE.Mesh(
-      new THREE.BoxBufferGeometry(60, 15, 30),
-      new THREE.MeshLambertMaterial({ color: 0xa52523 })
-    );
-    main.position.y = 12;
-    car.add(main);
-  
-    const carFrontTexture = getCarFrontTexture();
-  
-    const carBackTexture = getCarFrontTexture();
-  
-    const carRightSideTexture = getCarSideTexture();
-  
-    const carLeftSideTexture = getCarSideTexture();
-    carLeftSideTexture.center = new THREE.Vector2(0.5, 0.5);
-    carLeftSideTexture.rotation = Math.PI;
-    carLeftSideTexture.flipY = false;
-  
-    const cabin = new THREE.Mesh(new THREE.BoxBufferGeometry(33, 12, 24), [
-      new THREE.MeshLambertMaterial({ map: carFrontTexture }),
-      new THREE.MeshLambertMaterial({ map: carBackTexture }),
-      new THREE.MeshLambertMaterial({ color: 0xffffff }), // top
-      new THREE.MeshLambertMaterial({ color: 0xffffff }), // bottom
-      new THREE.MeshLambertMaterial({ map: carRightSideTexture }),
-      new THREE.MeshLambertMaterial({ map: carLeftSideTexture }),
-    ]);
-    cabin.position.x = -6;
-    cabin.position.y = 25.5;
-    car.add(cabin);
-  
-    return car;
-  }
